c105e7e4bdbc653579bc3980cde0e970f5a1b191
SarthAradhye <sartharadhye401@hotmail.com>
2025-02-16 22:51:37 +0530

    ai page

---
+++ products/ai.js
@@ -0,0 +1,6 @@
+function shaarda(){
+    window.location.href="https://shaarda.aradhytech.online"
+}
+const vishwalaksha=()=>{
+    window.location.href= "https://vishwalaksha.aradhyetech.online"
+}
