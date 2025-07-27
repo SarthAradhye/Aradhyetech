1454de3f97fab47d097c9f8dcd5e4ed9d746b9bf
SarthAradhye <sartharadhye@aradhyetech.online>
2025-07-27 21:54:17 +0530

    new website

--- products/ai.js
+++
@@ -1,6 +0,0 @@
-function shaarda(){
-    window.location.href="https://shaarda.aradhytech.online"
-}
-const vishwalaksha=()=>{
-    window.location.href= "https://vishwalaksha.aradhyetech.online"
-}
