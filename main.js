067340520fbd5402a616a115a0393f1b80f95759
SarthAradhye <sartharadhye@aradhyetech.online>
2025-10-08 21:42:03 +0530

    new website finally live

---
+++ main.js
@@ -0,0 +1,12 @@
+const date= new Date()
+
+const hours = date.getHours()
+const minutes = date.getMinutes()
+
+if (hours => 17){
+    var element = document.getElementsByClassName("backgroundImage");
+    element.style.backgroundColor ="#000138"
+}else{
+    var element = document.getElementsByClassName("backgroundImage");
+    element.style.backgroundColor ="#a4560dff"
+}
